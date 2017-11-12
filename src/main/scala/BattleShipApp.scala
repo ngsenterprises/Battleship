f66823bc7a149f9657af3de6aeaaf05270326ce8
ngsmithhotmailcom <ngsmith@hotmail.com>
2017-11-11 22:30:16 -0800

    remove comments

--- src/main/scala/BattleShipApp.scala
+++ src/main/scala/BattleShipApp.scala
@@ -6,12 +6,4 @@ object BattleShipApp extends App {
 
   println( s" hello battleship" )
 
-//  val numberOfPlayers = 2
-//  val players = List( "woody", "norwood" )
-//  val boardSize = (10, 10)
-//  val ships = List( (Carrier, 1), (Battleship, 1), (Cruiser, 1), (Submarine, 1), (Destroyer, 1) )
-
-
-
-
 }
